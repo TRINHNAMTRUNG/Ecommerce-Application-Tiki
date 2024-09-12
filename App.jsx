8c1587a0ce8ae947703a374d47da4e2a03199dfa
Ngoc <phamphuongngoc509@gmail.com>
2024-09-12 23:09:41 +0700

    merge

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import FirstScreen from './src/Screens/homePage.jsx';
+import FirstScreen from './src/pages/homePage';
 export default function App() {
   return (
     // <View style={styles.container}>
